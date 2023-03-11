1345e86f28572e5e15d5dae478051b052d2a1cb4
nguyentung2107 <nguyentung2107@gmail.com>
2023-03-11 10:38:38 -0800

    coding dau tien cua tui

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Hello world")
+
+//variable
+var age = 29;
+
+//array
+var names =["tung","tien","lana"];
+
+//object
+var House ={
+    door : 10,
+    table : 2,
+}
+console.log(House.table);
+//loop
+for (let i = 0; i<5;i++)
+{
+    console.log (i);
+}
+//print names
+for (let i =0 ; i<3;i++)
+{
+    console.log(names[i]);
+}
+
+
+//conditions - statements
+if(age <30)
+{
+console.log ("student")
+console.log("going to school")
+}
+else
+{
+    console.log("professor")
+}
+
+//function
+function Coffee_Maker (milk,coffee)
+{
+    return milk+coffee
+}
+console.log(Coffee_Maker(2,5))
+
